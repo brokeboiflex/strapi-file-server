b2aa03876ce9ab4c9a1c646d6d5439083d38a9c2
suko-mars <pytlewskimarcel@gmail.com>
2023-02-20 15:41:08 +0100

    get by path

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const { v4: uuidv4 } = require("uuid");
-// const path = require("path");
+const fs = require("fs");
+const path = require("path");
 
 // TODO
 // brotli /gzip
 // sprawdzanie plików po checksumach by nie zaśmiecać dysku
 
 const app = express();
-const port = 6661; // 777 jest liczba boga do którego prowadzi elixir, nodejs nie zasługuje
-
+const port = 6661; // 777 jest liczba boga  nodejs nie zasługuje
+const public = path.join(__dirname, "public");
 app.use(cors(), fileUpload());
-app.use(express.static("public"));
 
 app.post("/upload", async (req, res) => {
   if (!req.files) {
@@ -25,6 +25,15 @@ app.post("/upload", async (req, res) => {
     await req.files.file.mv(__dirname + "/public/" + newName);
     return res.status(200).send(newName);
   }
+});
+// app.delete("/files/:filename");
+
+app.get("/files/*", async (req, res) => {
+  console.log(req.url);
+  const pathToFile = req.url.substring(7, req.url.length);
+  console.log(pathToFile);
+  res.sendFile(path.join(public, pathToFile));
+  // const allFiles = res.send(allFiles);
 });
 
 app.listen(port, () => {
