9d7a4ae989e245c6deeea1876155c5ae25439186
suko-mars <pytlewskimarcel@gmail.com>
2023-02-20 16:48:14 +0100

    whitespace in name support

--- index.js
+++ index.js
@@ -20,20 +20,26 @@ app.post("/upload", async (req, res) => {
   if (!req.files) {
     return res.status(400).send("No files were uploaded.");
   } else {
-    const newName = uuidv4() + "." + req.files.file.name.split(".").pop();
-    console.log(req.files.file.name.split(".").pop());
-    await req.files.file.mv(__dirname + "/public/" + newName);
+    const fileName = req.files.file.name;
+    console.log(fileName);
+    const newName = uuidv4() + "." + fileName.split(".").pop();
+    // console.log(req.files.file.name.split(".").pop());
+    await req.files.file.mv(__dirname + "/public/" + fileName);
     return res.status(200).send(newName);
   }
 });
 // app.delete("/files/:filename");
 
+app.get("/listall", async (req, res) => {
+  // const allFiles =
+  // res.send(allFiles);
+});
+
 app.get("/files/*", async (req, res) => {
   console.log(req.url);
-  const pathToFile = req.url.substring(7, req.url.length);
+  const pathToFile = decodeURI(req.url.substring(7, req.url.length));
   console.log(pathToFile);
   res.sendFile(path.join(public, pathToFile));
-  // const allFiles = res.send(allFiles);
 });
 
 app.listen(port, () => {
