c93c638ac6cfc2d2ab2d53e0aa0e7b0d0059ef57
suko-mars <pytlewskimarcel@gmail.com>
2023-03-24 15:27:47 +0100

    dotenv is a bitch

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
-// import * as dotenv from "dotenv";
-// dotenv.config();
+const dotenv = require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 const fs = require("fs");
 const path = require("path");
 const hashes = require("./hashes");
-
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env"),
+  debug: true,
+});
 const app = express();
 const port = 6661;
-const public = path.join(__dirname, "../public");
+
+// Paths
+const publicFolder = path.join(__dirname, "../public");
+const protectedFolder = path.join(__dirname, "../public/protected");
+const tempFolder = path.join(__dirname, "../temp");
+
 const hashtablePath = path.join(__dirname, "../hashtable.txt");
-console.log(hashtablePath);
 
 const resolveFilePath = (req) =>
-  path.join(public, decodeURI(req.url.substring(7, req.url.length)));
+  path.join(publicFolder, decodeURI(req.url.substring(7, req.url.length)));
 app.use(cors(), fileUpload());
 
-const getAllFiles = (dirPath, arrayOfFiles) => {
-  files = fs.readdirSync(dirPath);
-  arrayOfFiles = arrayOfFiles || [];
+const getAllFiles = (dirPath: string, arrayOfFiles?: string[]) => {
+  const files = fs.readdirSync(dirPath);
+  let fileArray = arrayOfFiles ? arrayOfFiles : [];
   files.forEach(function (file) {
     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-      arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
+      fileArray = getAllFiles(dirPath + "/" + file, fileArray);
     } else {
       console.log(dirPath);
-      arrayOfFiles.push(
+      fileArray.push(
         path.join(__dirname, dirPath, "/", file).split("public/")[1]
       );
     }
@@ -34,19 +40,34 @@ const getAllFiles = (dirPath, arrayOfFiles) => {
   return arrayOfFiles;
 };
 
+console.log(process.env.DB_SERVER);
+// fetch(process.env.SERVER, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({ query: "{ hello }" }),
+// })
+//   .then((r) => r.json())
+//   .then((data) => console.log("data returned:", data));
+
 // TODO jeśli plik już jest na serwerze zwróć jego ścieżkę
 // jeśli plik jest bezpiecznie uploadowany a nie jest bezpiecznie przechowany przenieś go do bezpiecznego folderu
 // protected delete - może być wykonane jedynie z serwera gql jakoś po cors
 
 const upload = async (req, res, destination) => {
   // TODO jeżeli plik istnieje ale nie jest chroniony zabezpiecz go
+
+  console.log(process.env.SERVER_URL);
   try {
     if (!req.files) {
       return res.status(400).send("No files were uploaded.");
     } else {
       const fileName = await req.files.file.name;
-      const tempPath = path.join(__dirname, "../temp", fileName);
-      const constPath = path.join(__dirname, destination, fileName);
+      const tempPath = path.join(tempFolder, fileName);
+      const constPath = path.join(destination, fileName);
+
       await req.files.file.mv(tempPath);
       if (hashes.checkHash(tempPath, hashtablePath)) {
         fs.unlinkSync(tempPath);
@@ -63,10 +84,10 @@ const upload = async (req, res, destination) => {
 };
 
 app.post("/upload", async (req, res) => {
-  upload(req, res, "../public");
+  upload(req, res, publicFolder);
 });
 app.post("/protected-upload", async (req, res) => {
-  upload(req, res, "../public/protected");
+  upload(req, res, protectedFolder);
 });
 app.delete("/files/*", async (req, res) => {
   try {
@@ -85,7 +106,7 @@ app.delete("/files/*", async (req, res) => {
 });
 app.get("/listall", async (req, res) => {
   try {
-    const allFiles = getAllFiles(public);
+    const allFiles = getAllFiles(publicFolder);
     return res.status(200).send(allFiles);
   } catch (err) {
     throw new Error(err);
