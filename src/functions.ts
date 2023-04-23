1be91895f63534c08a0cde41b5c8e8bdd0a0cb0e
suko-mars <pytlewskimarcel@gmail.com>
2023-04-23 22:44:59 +0200

    fix

--- src/functions.ts
+++ src/functions.ts
@@ -32,7 +32,7 @@ export default function initFunctions(publicFolder: string) {
   };
 
   const resolveFilePath = async (req) => {
-    const fileName = decodeURI(req.url.substring(7, req.url.length));
+    const fileName = decodeURI(req.url).substring(7, req.url.length);
     const fileInfo = await getFileInfo(req, getFileByName, { name: fileName });
     if (fileInfo) {
       const { hash, extension } = fileInfo;
@@ -159,6 +159,86 @@ export default function initFunctions(publicFolder: string) {
           log("File already exists at this location", "red");
           return res.status(200).send(fileInfo);
         }
+      }
+    } catch (err) {
+      log("Upload error", "red");
+      throw new Error(err);
+    }
+  };
+
+  const copyFileFromURL = async (req, res) => {
+    try {
+      const authHeader = req.headers.authorization;
+      const client = createClient({
+        url: process.env.DB_SERVER,
+        // exchanges: [dedupExchange, cacheExchange, fetchExchange],
+        fetchOptions: () => {
+          return {
+            headers: { authorization: authHeader ? authHeader : "" },
+          };
+        },
+      });
+
+      const folder = req.body.folder;
+      const URL = req.body.url;
+
+      const fileName = decodeURI(req.files.file.name);
+      const tempPath = path.join(tempFolder, fileName);
+      await req.files.file.mv(tempPath);
+      let extension = "";
+      const extensionFromName = path.extname(tempPath);
+      if (extensionFromName) {
+        extension = extensionFromName;
+      } else {
+        const { fileTypeFromFile } = await import("file-type");
+        extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
+      }
+      const hash = hashes.getFileHash(tempPath);
+      // filename is its hash + extension to avoid storing duplicate files with different names
+      const constPath = path.join(publicFolder, hash + extension);
+      let fileInfo = await getFileInfo(req, getFileByHash, { hash });
+
+      if (fileInfo && fileInfo.id) {
+        log("File already uploaded", "blue");
+        fs.unlinkSync(tempPath);
+      } else {
+        log("Saving file", "blue");
+        fs.renameSync(tempPath, constPath);
+      }
+      if (!fileInfo || folder !== fileInfo.folder) {
+        const fileSizeInBytes =
+          fileInfo && typeof fileInfo.size === "number"
+            ? fileInfo.size
+            : fs.statSync(constPath).size;
+
+        const category =
+          fileInfo && fileInfo.category
+            ? fileInfo.category
+            : extensionToCategotry(extension);
+
+        const fileData = {
+          name: fileName,
+          extension: extension,
+          hash: hash,
+          size: fileSizeInBytes,
+          category: category,
+          last_modified: new Date().toISOString(),
+          path: folder,
+          //
+          typename: "File",
+        };
+        const mutationResult = await client
+          .mutation(createFile, { data: fileData })
+          .toPromise();
+        const error = mutationResult.error && mutationResult.error.message;
+        if (error) {
+          throw new Error(error);
+        }
+        fileInfo = mutationResult.data && mutationResult.data.createFile;
+        return res.status(201).send(fileInfo);
+      } else {
+        log("File already exists at this location", "red");
+        return res.status(200).send(fileInfo);
       }
     } catch (err) {
       log("Upload error", "red");
