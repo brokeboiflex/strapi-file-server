0abf7288c6cc60fb82930124f7e64eb5fc05767e
brokeboiflex <pytlewskimarcel@gmail.com>
2023-09-05 00:21:45 +0200

    fileupload fixed

--- src/functions.ts
+++ src/functions.ts
@@ -10,10 +10,9 @@ import _ from "lodash";
 
 // import { fileTypeFromFile } from "file-type";
 
-const tempFolder = path.join(__dirname, "../temp");
-
 export default function initFunctions(
   publicFolder: string,
+  tempFolder: string,
   apiType: "REST" | "GQL"
 ) {
   const { createFile, getFileByHash, getFileById } = gqlApi;
@@ -32,14 +31,17 @@ export default function initFunctions(
 
     const queryResult = await client.query(query, variables).toPromise();
     const error = queryResult.error && queryResult.error.message;
+    console.log(Object.values(queryResult.data)[0]["data"]);
+
     if (error) {
       throw new Error(error);
     }
-    let fileInfo: any =
-      (await queryResult.data) &&
-      (await Object.values(queryResult.data)[0]["data"]);
+    let fileInfo: any = await Object.values(queryResult.data)[0]["data"];
 
-    fileInfo = { id: fileInfo.id, ...fileInfo.attributes };
+    if (fileInfo) {
+      fileInfo = { id: fileInfo.id, ...fileInfo.attributes };
+    }
+
     return fileInfo;
   };
 
@@ -129,12 +131,16 @@ export default function initFunctions(
           extension = extensionFromName;
         } else {
           const { fileTypeFromFile } = await import("file-type");
-          extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
+          const filetype = await fileTypeFromFile(tempPath);
+          if (filetype) {
+            extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
+          }
         }
         const hash = hashes.getFileHash(tempPath);
         // filename is its hash + extension to avoid storing duplicate files with different names
         const constPath = path.join(publicFolder, hash + extension);
         let fileInfo = await getFileInfo(req, getFileByHash, { hash });
+        console.log(fileInfo);
 
         if (fileInfo && fileInfo.id) {
           log("File already uploaded", "blue");
@@ -176,86 +182,6 @@ export default function initFunctions(
           log("File already exists at this location", "red");
           return res.status(200).send(fileInfo);
         }
-      }
-    } catch (err) {
-      log("Upload error", "red");
-      throw new Error(err);
-    }
-  };
-
-  const copyFileFromURL = async (req, res) => {
-    try {
-      const authHeader = req.headers.authorization;
-      const client = createClient({
-        url: process.env.DB_SERVER,
-        // exchanges: [dedupExchange, cacheExchange, fetchExchange],
-        fetchOptions: () => {
-          return {
-            headers: { authorization: authHeader ? authHeader : "" },
-          };
-        },
-      });
-
-      const folder = req.body.folder;
-      const URL = req.body.url;
-
-      const fileName = decodeURI(req.files.file.name);
-      const tempPath = path.join(tempFolder, fileName);
-      await req.files.file.mv(tempPath);
-      let extension = "";
-      const extensionFromName = path.extname(tempPath);
-      if (extensionFromName) {
-        extension = extensionFromName;
-      } else {
-        const { fileTypeFromFile } = await import("file-type");
-        extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
-      }
-      const hash = hashes.getFileHash(tempPath);
-      // filename is its hash + extension to avoid storing duplicate files with different names
-      const constPath = path.join(publicFolder, hash + extension);
-      let fileInfo = await getFileInfo(req, getFileByHash, { hash });
-
-      if (fileInfo && fileInfo.id) {
-        log("File already uploaded", "blue");
-        fs.unlinkSync(tempPath);
-      } else {
-        log("Saving file", "blue");
-        fs.renameSync(tempPath, constPath);
-      }
-      if (!fileInfo || folder !== fileInfo.folder) {
-        const fileSizeInBytes =
-          fileInfo && _.isNumber(fileInfo.size)
-            ? fileInfo.size
-            : fs.statSync(constPath).size;
-
-        const category =
-          fileInfo && fileInfo.category
-            ? fileInfo.category
-            : extensionToCategotry(extension);
-
-        const fileData = {
-          name: fileName,
-          extension: extension,
-          hash: hash,
-          size: fileSizeInBytes,
-          category: category,
-          last_modified: new Date().toISOString(),
-          path: folder,
-          publishedAt: new Date().toISOString(),
-        };
-        const mutationResult = await client
-          .mutation(createFile, { data: fileData })
-          .toPromise();
-        const error = mutationResult.error && mutationResult.error.message;
-        if (error) {
-          throw new Error(error);
-        }
-        console.log(mutationResult);
-
-        return res.status(201).send(fileInfo);
-      } else {
-        log("File already exists at this location", "red");
-        return res.status(200).send(fileInfo);
       }
     } catch (err) {
       log("Upload error", "red");
