be1e88999160c7d71cfe4804903ddd636026179e
brokeboiflex <pytlewskimarcel@gmail.com>
2023-09-04 23:18:54 +0200

    Revert "im an idiot"
    This reverts commit 65d2647cdea4044202abefa1559eefd91d7c7361.

--- src/functions.ts
+++ src/functions.ts
@@ -3,16 +3,17 @@ import path from "path";
 import { createClient } from "@urql/core";
 import checkDiskSpace from "check-disk-space";
 
-import hashes from "../hashes";
-import gqlApi from "../api";
-import log from "../log";
+import hashes from "./hashes";
+import gqlApi from "./api";
+import log from "./log";
 import _ from "lodash";
 
 // import { fileTypeFromFile } from "file-type";
 
+const tempFolder = path.join(__dirname, "../temp");
+
 export default function initFunctions(
   publicFolder: string,
-  tempFolder: string,
   apiType: "REST" | "GQL"
 ) {
   const { createFile, getFileByHash, getFileById } = gqlApi;
@@ -34,8 +35,6 @@ export default function initFunctions(
     if (error) {
       throw new Error(error);
     }
-    console.log(queryResult);
-
     let fileInfo: any =
       (await queryResult.data) &&
       (await Object.values(queryResult.data)[0]["data"]);
@@ -109,6 +108,7 @@ export default function initFunctions(
         return res.status(400).send("No files were uploaded.");
       } else {
         const authHeader = req.headers.authorization;
+
         const client = createClient({
           url: process.env.DB_SERVER,
           // exchanges: [dedupExchange, cacheExchange, fetchExchange],
@@ -183,85 +183,85 @@ export default function initFunctions(
     }
   };
 
-  // const copyFileFromURL = async (req, res) => {
-  //   try {
-  //     const authHeader = req.headers.authorization;
-  //     const client = createClient({
-  //       url: process.env.DB_SERVER,
-  //       // exchanges: [dedupExchange, cacheExchange, fetchExchange],
-  //       fetchOptions: () => {
-  //         return {
-  //           headers: { authorization: authHeader ? authHeader : "" },
-  //         };
-  //       },
-  //     });
+  const copyFileFromURL = async (req, res) => {
+    try {
+      const authHeader = req.headers.authorization;
+      const client = createClient({
+        url: process.env.DB_SERVER,
+        // exchanges: [dedupExchange, cacheExchange, fetchExchange],
+        fetchOptions: () => {
+          return {
+            headers: { authorization: authHeader ? authHeader : "" },
+          };
+        },
+      });
 
-  //     const folder = req.body.folder;
-  //     const URL = req.body.url;
+      const folder = req.body.folder;
+      const URL = req.body.url;
 
-  //     const fileName = decodeURI(req.files.file.name);
-  //     const tempPath = path.join(tempFolder, fileName);
-  //     await req.files.file.mv(tempPath);
-  //     let extension = "";
-  //     const extensionFromName = path.extname(tempPath);
-  //     if (extensionFromName) {
-  //       extension = extensionFromName;
-  //     } else {
-  //       const { fileTypeFromFile } = await import("file-type");
-  //       extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
-  //     }
-  //     const hash = hashes.getFileHash(tempPath);
-  //     // filename is its hash + extension to avoid storing duplicate files with different names
-  //     const constPath = path.join(publicFolder, hash + extension);
-  //     let fileInfo = await getFileInfo(req, getFileByHash, { hash });
+      const fileName = decodeURI(req.files.file.name);
+      const tempPath = path.join(tempFolder, fileName);
+      await req.files.file.mv(tempPath);
+      let extension = "";
+      const extensionFromName = path.extname(tempPath);
+      if (extensionFromName) {
+        extension = extensionFromName;
+      } else {
+        const { fileTypeFromFile } = await import("file-type");
+        extension = `.${(await fileTypeFromFile(tempPath)).ext}`;
+      }
+      const hash = hashes.getFileHash(tempPath);
+      // filename is its hash + extension to avoid storing duplicate files with different names
+      const constPath = path.join(publicFolder, hash + extension);
+      let fileInfo = await getFileInfo(req, getFileByHash, { hash });
 
-  //     if (fileInfo && fileInfo.id) {
-  //       log("File already uploaded", "blue");
-  //       fs.unlinkSync(tempPath);
-  //     } else {
-  //       log("Saving file", "blue");
-  //       fs.renameSync(tempPath, constPath);
-  //     }
-  //     if (!fileInfo || folder !== fileInfo.folder) {
-  //       const fileSizeInBytes =
-  //         fileInfo && _.isNumber(fileInfo.size)
-  //           ? fileInfo.size
-  //           : fs.statSync(constPath).size;
+      if (fileInfo && fileInfo.id) {
+        log("File already uploaded", "blue");
+        fs.unlinkSync(tempPath);
+      } else {
+        log("Saving file", "blue");
+        fs.renameSync(tempPath, constPath);
+      }
+      if (!fileInfo || folder !== fileInfo.folder) {
+        const fileSizeInBytes =
+          fileInfo && _.isNumber(fileInfo.size)
+            ? fileInfo.size
+            : fs.statSync(constPath).size;
 
-  //       const category =
-  //         fileInfo && fileInfo.category
-  //           ? fileInfo.category
-  //           : extensionToCategotry(extension);
+        const category =
+          fileInfo && fileInfo.category
+            ? fileInfo.category
+            : extensionToCategotry(extension);
 
-  //       const fileData = {
-  //         name: fileName,
-  //         extension: extension,
-  //         hash: hash,
-  //         size: fileSizeInBytes,
-  //         category: category,
-  //         last_modified: new Date().toISOString(),
-  //         path: folder,
-  //         publishedAt: new Date().toISOString(),
-  //       };
-  //       const mutationResult = await client
-  //         .mutation(createFile, { data: fileData })
-  //         .toPromise();
-  //       const error = mutationResult.error && mutationResult.error.message;
-  //       if (error) {
-  //         throw new Error(error);
-  //       }
-  //       console.log(mutationResult);
+        const fileData = {
+          name: fileName,
+          extension: extension,
+          hash: hash,
+          size: fileSizeInBytes,
+          category: category,
+          last_modified: new Date().toISOString(),
+          path: folder,
+          publishedAt: new Date().toISOString(),
+        };
+        const mutationResult = await client
+          .mutation(createFile, { data: fileData })
+          .toPromise();
+        const error = mutationResult.error && mutationResult.error.message;
+        if (error) {
+          throw new Error(error);
+        }
+        console.log(mutationResult);
 
-  //       return res.status(201).send(fileInfo);
-  //     } else {
-  //       log("File already exists at this location", "red");
-  //       return res.status(200).send(fileInfo);
-  //     }
-  //   } catch (err) {
-  //     log("Upload error", "red");
-  //     throw new Error(err);
-  //   }
-  // };
+        return res.status(201).send(fileInfo);
+      } else {
+        log("File already exists at this location", "red");
+        return res.status(200).send(fileInfo);
+      }
+    } catch (err) {
+      log("Upload error", "red");
+      throw new Error(err);
+    }
+  };
 
   const deleteFile = async (req, res) => {
     try {
